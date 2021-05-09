be9bf14d069dee470202507332ba1b0f91cb751d
tenthent <kayratsagynbekov@gmail.com>
2021-05-09 16:55:40 +0600

    web-app

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.8
 RUN mkdir -p /usr/src/app/
 WORKDIR /usr/src/app/
 COPY . /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
 CMD ["python", "app.py"]
 #ENTRYPOINT ["python", "app.py"] - работает как CMD но без shell
--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-import time
+import os
+import json
+import datetime
+from flask import Flask
 
-while True:
-    print("Hello World")
-    time.sleep(1)
+app = Flask(__name__)
+
+BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
+RESOURSE_DIR = os.path.join(BASE_FOLDER, "resources")
+
+@app.route('/')
+def hello_world():
+    with open(os.path.join(RESOURSE_DIR, "response.json")) as f:
+        return "%s = %s" % (json.loads(f.read()).get("payload"),
+                            datetime.datetime.now().strftime("%d.%m.%Y "
+                                                             "%H:%M:%S"))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
